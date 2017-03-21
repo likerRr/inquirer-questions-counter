f326365ee4020486a53973fcbc56cf19328d7f91
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-03-21 15:54:58 +0300

    Add test for new feature

--- test.js
+++ test.js
@@ -61,3 +61,31 @@ test('custom format', t => {
 
 	t.is(q1.message, `1 from 1, What's your name?`);
 });
+
+test.skip(`don't change message of question which is not asked`, t => {
+	const q1 = {
+		type: 'input',
+		name: 'name',
+		message: `What's your name?`,
+		when: false
+	};
+
+	const q2 = {
+		type: 'input',
+		name: 'name',
+		message: `What's your nickname?`,
+		when: () => false
+	};
+
+	const q3 = {
+		type: 'input',
+		name: 'name',
+		message: `What's your email?`,
+	};
+
+	addCounter([q1, q2, q3]);
+
+	t.is(q1.message, `What's your name?`);
+	t.is(q2.message, `What's your nickname?`);
+	t.is(q3.message, `1/1 What's your email?`);
+});
